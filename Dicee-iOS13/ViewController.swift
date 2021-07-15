c480648ab84a2690ed2f954f75e2f645f36ee343
Pragya Awasthi <71930683+pragya2002@users.noreply.github.com>
2021-07-15 11:32:56 +0530

    Add files via upload
    initial

---
+++ Dicee-iOS13/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+       
+    }
+
+    @IBAction func buttonPressed(_ sender: Any) {
+       
+        let arr=[#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")]
+        diceImageView1.image=arr.randomElement()  //you can simply use int.random(in::0...5) instead of elements
+        diceImageView2.image=arr.randomElement()
+        
+    }
+    
+}
+
